5552144e565db3809dae2a08e07e67e6102ad6d7
Rob Hess <robwhess@gmail.com>
2017-11-27 09:48:15 -0800

    Add code from 11/20 lecture.

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ app.set('view engine', 'handlebars');
 
 // console.log("==peopleData:", peopleData);
 
-var availablePeople = [
-  'luke',
-  'beyonce',
-  'einstein',
-  'ta-nehisi',
-  'marie'
-]
+app.get('/', function (req, res) {
+  res.render('homePage');
+});
 
 app.get('/people', function (req, res) {
   res.status(200).sendFile(path.join(__dirname, 'public', 'people.html'));
@@ -24,7 +20,7 @@ app.get('/people', function (req, res) {
 
 app.get('/people/:personId', function(req, res, next) {
   var personId = req.params.personId;
-  if (availablePeople.indexOf(personId) !== -1) {
+  if (peopleData[personId]) {
     var person = peopleData[personId];
     res.status(200).render('personPage', person);
   }
@@ -36,7 +32,8 @@ app.get('/people/:personId', function(req, res, next) {
 app.use(express.static('public'));
 
 app.use('*', function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  // res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(8000, function () {
